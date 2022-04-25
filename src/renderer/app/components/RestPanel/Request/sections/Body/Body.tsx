c0e195a5eb0ecb8aed0258bb262636894136bc55
cooperrunyan <cooperrunyanspam@gmail.com>
2022-04-25 11:37:59 -0700

    Body saving work in progress

--- src/renderer/app/components/RestPanel/Request/sections/Body/Body.tsx
+++ src/renderer/app/components/RestPanel/Request/sections/Body/Body.tsx
@@ -1,9 +1,12 @@
 import style from './Body.module.scss';
 import { Controlled as CodeMirror } from 'react-codemirror2';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, useContext } from 'react';
 import { Bang } from 'renderer/app/types/Bang';
 import { getRequest } from 'renderer/app/db/get/request';
 import { useWindowSize } from 'usehooks-ts';
+import { modifyCurrentRequest } from 'renderer/app/db/modify/request';
+import { useDebounce } from 'usehooks-ts';
+import { RefresherContext } from 'renderer/app/Refresher';
 
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/mode/yaml/yaml';
@@ -23,14 +26,25 @@ export const Body: React.FC<Props> = ({ request, containerSize, updateInterval }
   const [value, setValue] = useState('');
   const size = useWindowSize();
   const container = useRef<HTMLDivElement>(null);
+  const { refresh, iteration } = useContext(RefresherContext);
+
+  const debouncedValue = useDebounce<string>(value, 40);
+
+  useEffect(() => void modifyCurrentRequest({ body: debouncedValue }).then(refresh), [debouncedValue]);
 
   useEffect(() => setValue(request.body || ''), []);
   useEffect(() => {
     const codeMirrorElement = container.current?.querySelector('.CodeMirror') as HTMLDivElement;
+
     codeMirrorElement.style.setProperty('height', `0px`);
+    codeMirrorElement.style.setProperty('width', `0px`);
+
     const height = container.current?.children[0]?.clientHeight;
+    const width = container.current?.children[0]?.clientWidth;
+
     codeMirrorElement.style.setProperty('height', `${height}px`);
-  }, [size, containerSize, container.current?.clientHeight, container.current?.clientWidth, updateInterval]);
+    codeMirrorElement.style.setProperty('width', `${width}px`);
+  }, [size.height, size.width, containerSize.height, containerSize.width, container.current?.clientHeight, container.current?.clientWidth, updateInterval]);
 
   return (
     <div className={style.Body} ref={container}>
