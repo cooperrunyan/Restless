a03e3787ce7c5dc9c5606ca3c93ee80841a2eb89
cooperrunyan <cooperrunyanspam@gmail.com>
2022-04-26 08:51:47 -0700

    Headers component styling

--- packages/renderer/src/app/components/RestPanel/Request/sections/Headers/Headers.tsx
+++ packages/renderer/src/app/components/RestPanel/Request/sections/Headers/Headers.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from 'react';
 import { AddOutline, CheckboxOutline, TrashOutline } from 'react-ionicons';
+import { useElementSize } from 'usehooks-ts';
 import { getRequest } from '../../../../../db/get/request';
 import { Bang } from '../../../../../types/Bang';
 import style from './Headers.module.scss';
@@ -8,33 +10,52 @@ interface Props {
 }
 
 export const Headers: React.FC<Props> = ({ request }) => {
+  const [ref, { width }] = useElementSize<HTMLDivElement>();
+
+  useEffect(() => {
+    const grid = document.querySelector<HTMLDivElement>(`.${style.Headers} .${style.grid}`)!;
+    if (width < 480) return grid.style.setProperty('grid-template-columns', '1fr 1fr');
+    return grid.style.setProperty('grid-template-columns', '');
+  }, [width]);
+
   if (!request) return <></>;
 
   return (
     <div className={style.Headers}>
-      <div className={style.grid}>
-        <div className={style.head}></div>
+      <div className={style.grid} ref={ref}>
+        {width >= 480 && <div className={style.head}></div>}
         <div className={style.head}>Header</div>
         <div className={style.head}>Value</div>
-        <div className={style.head + ' ' + style.add}>
-          <AddOutline color="var(--7)" width="2rem" height="2rem" />
-        </div>
+        {width >= 480 && (
+          <div className={style.head + ' ' + style.add}>
+            <AddOutline color="var(--7)" width="2rem" height="2rem" />
+          </div>
+        )}
 
-        <div className={style.spacer + ' ' + style.start}></div>
-        <div>
-          <input type="text" placeholder="header" />
-        </div>
-        <div>
-          <input type="text" placeholder="value" />
-          <span>
-            <CheckboxOutline color="var(--primary)" height="2rem" width="2rem" />
-          </span>
-          <span className={style.end}>
-            <TrashOutline color="var(--4)" height="2rem" width="2rem" />
-          </span>
-        </div>
-        <div className={style.spacer + ' ' + style.end}></div>
+        <Row tooSmall={width < 480} />
+        <Row tooSmall={width < 480} />
       </div>
     </div>
+  );
+};
+
+const Row: React.FC<{ tooSmall: boolean }> = ({ tooSmall }) => {
+  return (
+    <>
+      {!tooSmall && <div className={style.spacer + ' ' + style.start}></div>}
+      <div>
+        <input type="text" placeholder="header" />
+      </div>
+      <div>
+        <input type="text" placeholder="value" />
+        <span className={style.button}>
+          <CheckboxOutline color="var(--primary)" height="2rem" width="2rem" />
+        </span>
+        <span className={style.button + ' ' + style.end}>
+          <TrashOutline color="var(--4)" height="2rem" width="2rem" />
+        </span>
+      </div>
+      {!tooSmall && <div className={style.spacer + ' ' + style.end}></div>}
+    </>
   );
 };
